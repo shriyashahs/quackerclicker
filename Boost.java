b7e5a9e629b77d35897b23e3773e8e5608fb4fc4
Dana <danac00000@gmail.com>
2025-11-01 15:22:03 -0400

    new

---
+++ Boost.java
@@ -0,0 +1,3 @@
+public abstract Boost() {
+    private double MULTIPLIER_BOOST;
+}
---
+++ Quacks.java
@@ -0,0 +1,9 @@
+public class Quacks() {
+    private double QPS; //quacks per second
+    private double QPC; //quacks per click
+    private double multiplier;
+
+    public Quacks() {
+        
+    }
+}
