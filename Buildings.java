ee0afb341cd619283114c5de63e0594ed677a4dd
Shriya Shah <shriyashah@shriyas-mbp.wireless.rit.edu>
2025-11-01 16:40:50 -0400

    building and quacks

--- Buildings.java
+++ Buildings.java
@@ -1,10 +1,26 @@
-public abstract class Buildings {
+/**
+ * Abstract class which every building extends from
+ */
+public abstract class Buildings extends Quacks {
     private final int BASE_COST;
-    private final double MULTIPLICITY;
-
-    public Buildings(int base, double multiplier) {
+    private final double MULTIPLICITY; // value that cost of building increases by (ex. 15% or 0.15)
+    private double qps; // quacks per second
+    private double qpc; // quacks per click
+    private int counter; //counts # of buildings
+    
+    public Buildings(double multiplier, int base) {
+        super();
         BASE_COST = base;
         MULTIPLICITY = multiplier;
-    }    
-    
+        counter++;
+    }
+
+    /**
+     * Takes building count and raises 1.15 to the power of building count. Returns the result
+     * 
+     */
+    public double multiplier(int building_count) {
+        double multiplier = Math.pow(1.15, building_count);
+        return multiplier;
+    }
 }
--- Quacks.java
+++ Quacks.java
@@ -3,9 +3,47 @@ public class Quacks {
     private double QPC; //quacks per click
     private double multiplier;
 
+    public Quacks() {
+        QPS = 0;
+        QPC = 1;
+        multiplier = 1;
+    }
+
+    /**
+     * constrcutor for quack
+     * @param QPS quacks per second
+     * @param QPC quacks per click
+     * @param multiplier the QPS = multiplier * QPS // update later once we actually figure it out
+     */
     public Quacks(double QPS, double QPC, double multiplier) {
         this.QPS = QPS;
         this.QPC = QPC;
         this.multiplier = multiplier;
     }
+
+    //getters and setters
+    public double getQPS() {
+        return QPS;
+    }
+
+    public void setQPS(double qPS) {
+        QPS = qPS;
+    }
+
+    public double getQPC() {
+        return QPC;
+    }
+
+    public void setQPC(double qPC) {
+        QPC = qPC;
+    }
+
+    public double getMultiplier() {
+        return multiplier;
+    }
+
+    public void setMultiplier(double multiplier) {
+        this.multiplier = multiplier;
+    }
+    
 }
