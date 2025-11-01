5c64553727ab17fec3ad867f20aefdbbcb7c9a77
Dana <danac00000@gmail.com>
2025-11-01 15:24:25 -0400

    buildings

--- Buildings.java
+++ Buildings.java
@@ -1,3 +1,11 @@
 public abstract Buildings() {
+    private final int BASE_COST;
+    private final double MULTIPLICITY;
+
+    Buildings(int base, double multiplier) {
+        BASE_COST = base;
+        MULTIPLICITY = multiplier;
+    }
+    
     
 }
