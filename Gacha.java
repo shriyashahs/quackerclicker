e6ad7662b933d6a1afd5ab4805f5fce648c5b457
Shriya Shah <shriyashah@shriyas-mbp.wireless.rit.edu>
2025-11-01 20:01:20 -0400

    host change

---
+++ Gacha.java
@@ -0,0 +1,8 @@
+public class Gacha {
+    /**
+     * pull method/class? generate random number and character
+     * make gems, which can be bought with cookies
+     * 
+     * 
+     */
+}
--- Game.java
+++ Game.java
@@ -1,6 +1,9 @@
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JLabel;
+import javax.swing.ImageIcon;
+import javax.swing.Icon;
 
 public class Game {
     public JFrame frame;
